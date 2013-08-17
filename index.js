0ba42dc17a056d954d07c2d2f75c20b987f15ace
Kevin Lindsey <kevin@kevlindev.com>
2013-08-17 15:11:44 -0500

    Add some tests and refactor a bit

--- index.js
+++ index.js
@@ -1,62 +1,115 @@
+/**
+ *
+ *  Class Utilties
+ *
+ *  copyright 2013, Kevin Lindsey
+ *
+ */
+
+/**
+ *  isObject
+ *
+ *  @param {any} item
+ *  @returns {Boolean}
+ */
 function isObject(item) {
-    return typeof item == "object"
+    return typeof item === "object"
 }
 
+/**
+ *  isFunction
+ *
+ *  @param {any} item
+ *  @returns {Boolean}
+ */
+function isFunction(item) {
+    return typeof item === "function"
+}
+
+/**
+ *  isString
+ *
+ *  @param {any} item
+ *  @returns {Boolean}
+ */
 function isString(item) {
-    return typeof item == "string"
+    return typeof item === "string"
 }
 
+/**
+ *  isMethod
+ *
+ *  @param {Object} item
+ *  @param {String} methodName
+ *  @returns {Boolean}
+ */
 function isMethod(item, methodName) {
-    return isObject(item) && isString(methodName) && methodName in item && typeof item[methodName] === "function";
+    return isObject(item) && isString(methodName) && methodName in item && isFunction(item[methodName]);
 }
 
-function extend(o, p) {
-    for (prop in p) {
-        o[prop] = p[prop];
+/**
+ *  mergeProperties
+ *
+ *  @param {Object} source
+ *  @param {Object} target
+ *  @return {Object}
+ */
+function mergeProperties(source, target) {
+    for (prop in source) {
+        target[prop] = source[prop];
     }
 
-    return o;
+    return target;
 }
 
-function inherit(p) {
-    if (p === null) {
+/**
+ *  inherit
+ *
+ *  @param {Object | Function} proto
+ *  @returns {Object}
+ */
+function inherit(proto) {
+    if (isObject(proto) === false && isFunction(proto) === false) {
         throw new TypeError();
     }
 
     if ("create" in Object) {
-        return Object.create(p);
+        return Object.create(proto);
     }
     else {
-        var type = typeof p;
-
-        if (t !== "object" && t !== "function") {
-            throw new TypeError();
-        }
-
         function f() {};
         f.prototype = p;
         return new f();
     }
 }
 
+/**
+ *  defineSubclass
+ *
+ *  @param {Object} superclass
+ *  @param {Object} constructor
+ *  @param {Object} methods
+ *  @param {Object} statics
+ */
 function defineSubclass(superclass, constructor, methods, statics) {
     constructor.prototype = inherit(superclass.prototype);
     constructor.prototype.constructor = constructor;
 
     if (methods) {
-        extend(constructor.prototype, methods);
+        mergeProperties(methods, constructor.prototype);
     }
 
     if (statics) {
-        extend(constructor, statics);
+        mergeProperties(statics, constructor);
     }
 
     return constructor;
 }
 
 exports.isObject = isObject;
+exports.isFunction = isFunction;
 exports.isString = isString;
 exports.isMethod = isMethod;
-exports.extend = extend;
+exports.mergeProperties = mergeProperties;
 exports.inherit = inherit;
 exports.defineSubclass = defineSubclass;
